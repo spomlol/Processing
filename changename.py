e7ff4248d3e88fcb0d757ec71465fcc9c57cf6bd
spomlol <cedric_hermans@hotmail.com>
2019-04-13 15:06:47 +0200

    Resized pictures => faster loading, less data usage

--- changename.py
+++ changename.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 
-import os;
+import os
+from PIL import Image
+from resizeimage import resizeimage
 files = os.listdir("Images-spain/")
 print(files)
 count = 1
-for i in files:
+""" for i in files:
     os.rename("Images-spain/"+i,"Images-spain/"+str(count)+".jpg")
+    count+=1 """
+
+for i in os.listdir("Images-spain/"):
+    img = Image.open("Images-spain/"+str(i))
+    img = resizeimage.resize_width(img, 400)
+    img.save("Images-spain/resized-"+str(count)+'.jpeg', "jpeg")
     count+=1
